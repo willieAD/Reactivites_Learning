27b07d5c8e3a4e8538719a28d61adb162dffad6d
myPC <wadams678@gmail.com>
2021-07-09 12:57:46 +1000

    add react

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,7 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
+
         public Startup(IConfiguration config)
         {
             _config = config;
@@ -27,16 +28,34 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+            services
+                .AddSwaggerGen(c =>
+                {
+                    c
+                        .SwaggerDoc("v1",
+                        new OpenApiInfo { Title = "API", Version = "v1" });
+                });
+            services
+                .AddDbContext<DataContext>(opt =>
+                {
+                    opt
+                        .UseSqlite(_config
+                            .GetConnectionString("DefaultConnection"));
+                });
+            services
+                .AddCors(opt =>
+                {
+                    opt
+                        .AddPolicy("CorsPolicy",
+                        policy =>
+                        {
+                            policy
+                                .AllowAnyMethod()
+                                .AllowAnyHeader()
+                                .WithOrigins("http://localhost:3000");
+                        });
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,19 +65,25 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                app
+                    .UseSwaggerUI(c =>
+                        c
+                            .SwaggerEndpoint("/swagger/v1/swagger.json",
+                            "API v1"));
             }
 
             // app.UseHttpsRedirection();
-
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
         }
     }
 }
